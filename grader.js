f3176c351c9db88abb290802fba18cb870db0ab5
Kristopher Wuollett <kristopher@wuollett.net>
2013-07-06 20:24:51 +0000

    Added url handling to grader.js.

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -35,6 +36,21 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertUrlExists = function(url) {
+    console.log("assertUrlExists(%s)", url);
+    var instr;
+    rest.get(url).on('complete', function(result) {
+	if (result instanceof Error) {
+	    console.log("%s retreival had error: %s. Exiting", result.message);
+	    process.exit(1);
+	}
+	instr = result;
+        console.log("Got result: %s", result);
+    });
+    console.log("Returning result: %s", instr);
+    return instr;
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -55,6 +71,31 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHtmlString = function(htmlString, checksFile) {
+    $ = cheerio.load(htmlString);
+    var checks = loadChecks(checksFile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+}
+
+
+var checkUrl = function(url, checksfile) {
+    rest.get(url).on('complete', function(result) {
+	if (result instanceof Error) {
+	    console.log("%s retreival had error: %s. Exiting", result.message);
+	    process.exit(1);
+	}
+        var checkJson = checkHtmlString(result, checksfile);
+	var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    });
+    
+ }
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +106,22 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Path to URL')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    console.log("program.file: %s", program.file);
+    console.log("program.url: %s", program.url);
+    if (typeof program.file === 'undefined' && typeof program.url === 'undefined') {
+	console.log("Either -url or -file must be specified. Exiting.");
+	process.exit(1);
+    }
+    if (typeof program.url != 'undefined') {
+	checkUrl(program.url, program.checks);
+    } else {
+	var fileStr = program.file || program.url;
+	var checkJson = checkHtmlFile(fileStr, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
